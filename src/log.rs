cfd62de8b4083a222924558c9837416902ea2659
Mar <mar@strawmelonjuice.com>
2024-03-21 16:06:40 +0100

    A start, i guess.

--- src/log.rs
+++ src/log.rs
@@ -1,9 +1,9 @@
 use colored::Colorize;
 
 pub fn error(msg: String) {
-    eprintln!("{0}: {msg}", "ERROR".red())
+    eprintln!("{0}: {msg}", "error".red())
 }
 
 pub fn info(msg: String) {
-    println!("{0}: {msg}", "INFO".bright_yellow())
+    println!("{0}: {msg}", "info".bright_yellow())
 }
