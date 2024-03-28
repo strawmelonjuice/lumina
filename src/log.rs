dc1ee4b62dd8aa19b1a2838d244a218e92bd4e49
Mar <mar@strawmelonjuice.com>
2024-03-29 00:24:58 +0100

    Some changes to logging.

--- src/log.rs
+++
@@ -1,9 +0,0 @@
-use colored::Colorize;
-
-pub fn error(msg: String) {
-    eprintln!("{0}: {msg}", "error".red())
-}
-
-pub fn info(msg: String) {
-    println!("{0}: {msg}", "info".bright_yellow())
-}
