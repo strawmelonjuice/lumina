114fbe97b9d7c1c361021a232a6753a4ec6813a7
Mar <mar@strawmelonjuice.com>
2024-04-15 18:52:05 +0200

    Drop CSV support

--- src/storage.rs
+++ src/storage.rs
@@ -5,11 +5,8 @@
  */
 
 use rusqlite::Connection;
-use std::fs;
-use std::fs::File;
-use std::io::Write;
+use std::{process};
 use std::io::{Error, ErrorKind};
-use std::path::Path;
 
 use serde::{Deserialize, Serialize};
 
@@ -84,70 +81,17 @@ pub fn fetch(
                 })
             })
             .unwrap();
-        return match res.next() {
+        match res.next() {
             None => Ok(None),
             Some(r) => match r {
                 Ok(s) => Ok(Some(s)),
                 _ => Err(Error::new(ErrorKind::Other, "Unparseable data.")),
             },
-        };
-    }
-    match table.as_str() {
-        "users" => {
-            let mut userlist: Vec<TableUsers> = Vec::new();
-            match config.database.method.as_str() {
-                "csv" => {
-                    if !Path::new("./data").exists() {
-                        fs::create_dir_all("./data")?;
-                    }
-                    if !Path::new("./data/users.csv").exists() {
-                        File::create("./data/users.csv")
-                            .and_then(|mut d| writeln!(d, "id,username,password"))?;
-                    }
-                    let mut rdr = csv::Reader::from_path("./data/users.csv")?;
-                    for x in rdr.records() {
-                        let a = x.unwrap();
-                        let d = TableUsers {
-                            id: a.get(0).unwrap().parse().unwrap(),
-                            username: a.get(1).unwrap().parse().unwrap(),
-                            password: a.get(2).unwrap().parse().unwrap(),
-                        };
-                        userlist.push(d);
-                    }
-                }
-                _ => {
-                    return Err(Error::new(
-                        ErrorKind::InvalidData,
-                        "Unknown database method!",
-                    ))
-                }
-            };
-            // This can be done better, by calling it in SQL. But I think I might be a little lazy. (plus CSV support!)
-            // Update: SQLITE now returns early.
-            for user in userlist {
-                match searchr {
-                    "username" => {
-                        if user.username == searchv {
-                            return Ok(Some(serde_json::to_string(&user)?));
-                        }
-                    }
-                    "password" => {
-                        if user.password == searchv {
-                            return Ok(Some(serde_json::to_string(&user)?));
-                        }
-                    }
-                    "id" => {
-                        if user.id.to_string() == searchv {
-                            return Ok(Some(serde_json::to_string(&user)?));
-                        }
-                    }
-                    _ => return Err(Error::new(ErrorKind::InvalidData, "Unknown search method!")),
-                }
-            }
         }
-        _ => return Err(Error::new(ErrorKind::InvalidData, "Unknown table!")),
-    };
-    Ok(None)
+    } else {
+        error!("Unknown or unsupported database type! Only SQLITE is supported as of now.");
+        process::exit(1);
+    }
 }
 fn dbconf(conn: &Connection) {
     let emergencyabort = || {
