1e8cf36d900426ebda2169a8e189421048640f02
Mar <mar@strawmelonjuice.com>
2024-05-18 16:12:45 +0200

    Example post

--- src/database.rs
+++ src/database.rs
@@ -28,22 +28,22 @@ pub struct BasicUserInfo {
 #[derive(Debug, Serialize, Deserialize)]
 pub struct PostInfo {
     /// Local Post ID
-    lpid: i64,
+    pub lpid: i64,
     /// Post ID
-    pid: i64,
+    pub pid: i64,
     /// Instance ID - Might not be necessary on local posts. Hence being Option<>;
-    instance: Option<String>,
+    pub instance: Option<String>,
     /// Author ID
-    author_id: i64,
+    pub author_id: i64,
     /// Timestamp (UNIX)
-    timestamp: i64,
+    pub timestamp: i64,
     /// Content type
     // 1: Textual post (json<content: text/plain>)
     // 2: Article textual post (json<header: text/plain, body: text/markdown>)
     // 3: Media post (json<caption: text/markdown, media-id: integer[]>)
-    content_type: i32,
+    pub content_type: i32,
     /// Content in JSON, deserialised depending on content_type.
-    content: String,
+    pub content: String,
 }
 /// Create a database connection
 pub(crate) fn create_con(config: &Config) -> Connection {
@@ -71,7 +71,7 @@ pub fn fetch(
 ) -> Result<Option<String>, Error> {
     if config.database.method.as_str() == "sqlite" {
         match table.as_str() {
-            "Users" | "TimeLinePostPool" => {}
+            "Users" | "PostsStore" => {}
             _ => {
                 error!("Unknown table requisted!");
                 panic!("Unknown table requisted!");
@@ -79,9 +79,11 @@ pub fn fetch(
         };
         let conn = create_con(config);
         dbconf(&conn);
+
         let mut stmt = conn
-            .prepare(format!(r#"select * from {table} where {searchr} = "{searchv}""#).as_str())
+            .prepare(format!(r#"select * from {table} where {searchr} = '{searchv}'"#).trim())
             .unwrap();
+        debug!("{:?}", stmt);
         let mut res = stmt
             .query_map((), |row| {
                 Ok(match table.as_str() {
@@ -92,16 +94,18 @@ pub fn fetch(
                         email: row.get(3)?,
                     })
                     .unwrap(),
-                    "TimeLinePostPool" => serde_json::to_string(&PostInfo {
-                        pid: row.get(0)?,
-                        instance: row.get(1)?,
-                        author_id: row.get(2)?,
-                        timestamp: row.get(3)?,
-                        content_type: row.get(4)?,
-                        content: row.get(5)?,
-                        lpid: row.get(6)?,
-                    })
-                    .unwrap(),
+                    "PostsStore" => {
+                        let s = PostInfo {
+                            lpid: row.get(0)?,
+                            pid: row.get(1)?,
+                            instance: row.get(2)?,
+                            author_id: row.get(3)?,
+                            timestamp: row.get(4)?,
+                            content_type: row.get(5)?,
+                            content: row.get(6)?,
+                        };
+                        serde_json::to_string(&s).unwrap()
+                    }
                     _ => {
                         error!("Unknown table requisted!");
                         panic!("Unknown table requisted!");
@@ -109,11 +113,15 @@ pub fn fetch(
                 })
             })
             .unwrap();
+        // println!("{:?}", res.nth(0));
         match res.next() {
             None => Ok(None),
             Some(r) => match r {
                 Ok(s) => Ok(Some(s)),
-                _ => Err(Error::new(ErrorKind::Other, "Unparseable data.")),
+                Err(f) => {
+                    eprintln!("{:?}", f);
+                    Err(Error::new(ErrorKind::Other, "Unparseable data."))
+                }
             },
         }
     } else {
@@ -143,11 +151,11 @@ CREATE TABLE if not exists Users (
     };
     match conn.execute(
         "
-CREATE TABLE if not exists TimeLinePostPool (
+CREATE TABLE if not exists PostsStore (
     lpid            INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
     pid             INTEGER,
     instance        TEXT,
-    author_id      TEXT NOT NULL,
+    author_id      INTEGER NOT NULL,
     timestamp      INTEGER NOT NULL,
     content_type    INTEGER NOT NULL,
     content        TEXT NOT NULL
