d09b3f234f5ba1224020de96a51ddbb180cb6743
Mar <mar@strawmelonjuice.com>
2024-06-20 21:25:10 +0200

    Special dates + Style sheet inlining

--- src/serve.rs
+++ src/serve.rs
@@ -3,6 +3,68 @@
  *
  * Licensed under the BSD 3-Clause License. See the LICENSE file for more info.
  */
+const SPECIALDATES: &str = r#"
+.monthclass-6 .contentkeeper::before {
+    margin-top: .8em;
+    margin-bottom: .8em;
+    content: "Happy Pride Month!";
+    justify-content: center;
+    align-items: center;
+    height: 2.4em;
+    color: black;
+    width: 100%;
+    display: inline-flex;
+    background-image: linear-gradient(to right, rgb(237, 34, 36), rgb(243, 91, 34), rgb(249, 150, 33), rgb(245, 193, 30), rgb(241, 235, 27) 27%, rgb(241, 235, 27), rgb(241, 235, 27) 33%, rgb(99, 199, 32), rgb(12, 155, 73), rgb(33, 135, 141), rgb(57, 84, 165), rgb(97, 55, 155), rgb(147, 40, 142))
+}
+.dayclass-29.monthclass-2 .contentkeeper::before {
+    margin-top: .8em;
+    margin-bottom: .8em;
+    content: "[This day does not exist]";
+    justify-content: center;
+    align-items: center;
+    height: 2.4em;
+    flex: none;
+    color: yellow;
+    width: 100%;
+    display: inline-flex;
+    background-color: black;
+    text-shadow: 22px 4px 2px rgba(255,255,0,0.6);
+    box-shadow: 2px 2px 10px 8px #3d3a3a;
+    animation-name: glitched;
+    animation-duration: 3s;
+    animation-iteration-count: infinite;
+    animation-timing-function: linear;
+    animation-direction: alternate;
+}
+@keyframes glitched {
+    0% {
+        transform: skew(-20deg);
+        left: -4px;
+    }
+    10% {
+        transform: skew(-20deg);
+        left: -4px;
+    }
+    11% {
+        transform: skew(0deg);
+        left: 2px;
+    }
+    50% {
+        transform: skew(0deg);
+    }
+    51% {
+        transform: skew(10deg);
+    }
+    59% {
+        transform: skew(10deg);
+    }
+    60% {
+        transform: skew(0deg);
+    }
+    100% {
+        transform: skew(0deg);
+    }
+}"#;
 
 use actix_session::Session;
 use actix_web::http::header::LOCATION;
@@ -14,6 +76,7 @@ use tokio::sync::{Mutex, MutexGuard};
 
 use crate::database::{BasicUserInfo, IIExchangedUserInfo};
 use crate::{LuminaConfig, ServerVars};
+use chrono::Datelike;
 
 pub(super) async fn notfound(
     server_vars_mutex: Data<Mutex<ServerVars>>,
@@ -57,14 +120,40 @@ pub(super) async fn root(
 
     HttpResponse::build(StatusCode::OK)
         .content_type("text/html; charset=utf-8")
-        .body(
-            crate::assets::STR_ASSETS_INDEX_HTML
-                .replace(
-                    "{{iid}}",
-                    &server_vars.clone().config.interinstance.iid.clone(),
-                )
-                .clone(),
+        .body(replaceable(
+            crate::assets::STR_ASSETS_INDEX_HTML,
+            &server_vars,
+        ))
+}
+
+fn replaceable(string: &str, server_vars: &ServerVars) -> String {
+    // "Compress" that CSS too, however, be careful. Two consecutive spaces can be removed, but one on its own might mean something!
+    let specialdates = SPECIALDATES.replace(['\r', '\n'], "").replace("  ", "");
+    let current_date = chrono::Utc::now();
+    let mut stylesheet: String = String::new();
+    stylesheet.push_str("<style>");
+    stylesheet.push_str("\n\n\n/* --- Main stylesheet --- */\n\n\n");
+    stylesheet.push_str(crate::assets::STR_GENERATED_MAIN_MIN_CSS);
+    stylesheet.push_str("\n\n\n/* --- Custom instance-specific CSS content --- */\n\n\n");
+    stylesheet.push_str(&*server_vars.config.run.customcss.clone());
+    stylesheet.push_str("\n\n\n/* --- CSS content for special events --- */\n\n\n");
+    stylesheet.push_str(specialdates.as_str());
+    stylesheet.push_str("</style>");
+    let s = string
+        .replace(
+            "{{iid}}",
+            &server_vars.clone().config.interinstance.iid.clone(),
         )
+        .replace(
+            "monthclass-month",
+            format!("monthclass-{}", current_date.month()).as_str(),
+        )
+        .replace(
+            "dayclass-day",
+            format!("dayclass-{}", current_date.day()).as_str(),
+        )
+        .replace("<style></style>", &*stylesheet);
+    s.clone()
 }
 
 pub(super) async fn login(
@@ -83,14 +172,10 @@ pub(super) async fn login(
 
     HttpResponse::build(StatusCode::OK)
         .content_type("text/html; charset=utf-8")
-        .body(
-            crate::assets::STR_ASSETS_LOGIN_HTML
-                .replace(
-                    "{{iid}}",
-                    &server_vars.clone().config.interinstance.iid.clone(),
-                )
-                .clone(),
-        )
+        .body(replaceable(
+            crate::assets::STR_ASSETS_LOGIN_HTML,
+            &server_vars,
+        ))
 }
 
 pub(super) async fn signup(
@@ -109,14 +194,10 @@ pub(super) async fn signup(
 
     HttpResponse::build(StatusCode::OK)
         .content_type("text/html; charset=utf-8")
-        .body(
-            crate::assets::STR_ASSETS_SIGNUP_HTML
-                .replace(
-                    "{{iid}}",
-                    &server_vars.clone().config.interinstance.iid.clone(),
-                )
-                .clone(),
-        )
+        .body(replaceable(
+            crate::assets::STR_ASSETS_SIGNUP_HTML,
+            &server_vars,
+        ))
 }
 
 pub(super) async fn prefetch_js(
@@ -259,42 +340,6 @@ pub(super) async fn signup_js(
         .body(js)
 }
 
-pub(super) async fn site_c_css(
-    server_vars_mutex: Data<Mutex<ServerVars>>,
-    req: HttpRequest,
-) -> HttpResponse {
-    let server_vars = ServerVars::grab(&server_vars_mutex).await;
-    let config: LuminaConfig = server_vars.clone().config;
-    let coninfo = req.connection_info();
-    let ip = coninfo.realip_remote_addr().unwrap_or("<unknown IP>");
-    (server_vars.tell)(format!(
-        "{2}\t{:>45.47}\t\t{}",
-        "/custom.css".magenta(),
-        ip.yellow(),
-        "Request/200".bright_green()
-    ));
-    HttpResponse::build(StatusCode::OK)
-        .content_type("text/css; charset=utf-8")
-        .body(config.run.customcss)
-}
-
-pub(super) async fn site_css(
-    server_vars_mutex: Data<Mutex<ServerVars>>,
-    req: HttpRequest,
-) -> HttpResponse {
-    let server_vars = ServerVars::grab(&server_vars_mutex).await;
-    let coninfo = req.connection_info();
-    let ip = coninfo.realip_remote_addr().unwrap_or("<unknown IP>");
-    (server_vars.tell)(format!(
-        "{2}\t{:>45.47}\t\t{}",
-        "/site.css".magenta(),
-        ip.yellow(),
-        "Request/200".bright_green()
-    ));
-    HttpResponse::build(StatusCode::OK)
-        .content_type("text/css; charset=utf-8")
-        .body(crate::assets::STR_GENERATED_MAIN_MIN_CSS)
-}
 
 pub(super) async fn red_cross_svg(
     server_vars_mutex: Data<Mutex<ServerVars>>,
@@ -443,14 +488,10 @@ pub(super) async fn homepage(
             ));
             HttpResponse::build(StatusCode::OK)
                 .content_type("text/html; charset=utf-8")
-                .body(
-                    crate::assets::STR_ASSETS_HOME_HTML
-                        .replace(
-                            "{{iid}}",
-                            &server_vars.clone().config.interinstance.iid.clone(),
-                        )
-                        .clone(),
-                )
+                .body(replaceable(
+                    crate::assets::STR_ASSETS_HOME_HTML,
+                    &server_vars,
+                ))
         },
     )
     .await
