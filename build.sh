74d952fecf37542b40ce07934d3de61cbed5aabc
Mar <mar@strawmelonjuice.com>
2024-11-17 02:05:07 +0100

    some touch-ups

--- build.sh
+++ build.sh
@@ -3,78 +3,105 @@
 LOCA=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 SECONDS=0
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+noti() {
+    echo -e "\e[3m\e[1m$1\e[23m\e[22m"
+}
+errnoti() {
+	echo -e "\x1B[31m$1\e[0m"	
+}
+success() {
+	echo -e "\e[38;5;42m$1\e[39m"
+}
+
+
 rm -rf "$LOCA/backend/priv/generated/js"
 mkdir "$LOCA/backend/priv/generated/js"
 
 if [[ "$*" == *"--frontend-ts"* ]]; then
-	echo "Building front-end (TS)..."
-	cd "$LOCA/frontend-ts/"
+	noti "Building front-end (TS)..."
+	cd "$LOCA/frontend-ts/" || exit 1
 	bun install
 	bun build "$LOCA/frontend-ts/app.ts" --minify --target=browser --outdir "$LOCA/backend/priv/generated/js/" --sourcemap=linked --minify
 	bun "$LOCA/tobundle.ts" -- js-1 "$LOCA/backend/priv/generated/js/app.js"
 else
 	if [[ "$*" == *"--frontend-gleam"* ]]; then
-		echo "Building front-end (Gleam)..."
+		noti "Building front-end (Gleam)..."
 
-		cd "$LOCA/frontend/"
+		cd "$LOCA/frontend/" || exit 1
 		if gleam build --target js; then
-			echo "Frontend build success."
+			success "\t--> Frontend build success."
 		else
-			echo "Frontend compilation ran into an error."
+			errnoti "\t--> Frontend compilation ran into an error."
 			exit 1
 		fi
 		echo "import { main } from \"./frontend.mjs\";main();" >"$LOCA/frontend/build/dev/javascript/frontend/app.js"
 		bun build "$LOCA/frontend/build/dev/javascript/frontend/app.js" --minify --target=browser --outdir "$LOCA/backend/priv/generated/js/" --sourcemap=external --minify
 		bun "$LOCA/tobundle.ts" -- js-1 "$LOCA/backend/priv/generated/js/app.js"
 	else
-		echo "Invalid or missing frontend option, expected either \"--frontend-ts\" or \"--frontend-gleam\"."
+		errnoti "Invalid or missing frontend option, expected either \"--frontend-ts\" or \"--frontend-gleam\"."
 		exit 1
 	fi
 fi
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-echo "Front-end should be done. Continuing to generated assets."
-cd "$LOCA/backend/"
+noti "Front-end should be done. Continuing to generated assets."
+cd "$LOCA/backend/" || exit 1
 bun install
 rm -rf "$LOCA/backend/priv/generated/css/"
 mkdir "$LOCA/backend/priv/generated/css/"
-echo "Generating CSS... (TailwindCSS)"
+noti "Generating CSS... (TailwindCSS)"
 bun x postcss -o "$LOCA/backend/priv/generated/css/main.css" "$LOCA/backend/assets/styles/main.pcss" -u autoprefixer -u tailwindcss
 bun "$LOCA/tobundle.ts" -- css-1 "$LOCA/backend/priv/generated/css/main.css"
-echo "Minifying CSS..."
+noti "Minifying CSS..."
 bun x cleancss -O1 specialComments:all --inline none "$LOCA/backend/priv/generated/css/main.css" -o "$LOCA/backend/priv/generated/css/main.min.css"
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-echo "Starting on back-end compilation"
-cd "$LOCA/backend/"
+noti "Starting on back-end compilation"
+cd "$LOCA/backend/" || exit 1
 if gleam build --target erlang; then
-	echo "Success!"
+	success "\t--> Back-end build success."
 else
-	echo "Compilation ran into an error!"
+	errnoti "\t--> Compilation ran into an error!"
 	exit 1
 fi
 duration=$SECONDS
-echo "Build completed, took $((duration / 60)) minutes and $((duration % 60)) seconds."
+noti "Build completed, took $((duration / 60)) minutes and $((duration % 60)) seconds."
 if [[ "$*" == *"--run"* ]]; then
-	echo "'--run' detected. Running Lumina directly!"
+	noti "'--run' detected. Running Lumina directly!"
 	gleam run -- start
 else
 	if [[ "$*" == *"--pack"* ]]; then
-		echo "'--pack' detected. Packaging for deployment."
-else 
-	if [[ "$*" == *"--test"* ]]; then
-		echo "'--test' detected. Running backend tests."        				
-		cd "$LOCA/backend/"
-		gleam test --target erlang
-		echo "'--test' detected. Running frontend tests."        				
-
-		if [[ "$*" == *"--frontend-ts"* ]]; then
-        				cd "$LOCA/frontend-ts/"
-        				bun test 
-        else
-        	if [[ "$*" == *"--frontend-gleam"* ]]; then
-        				cd "$LOCA/frontend/"
-						gleam test --target javascript
+		noti "'--pack' detected. Packaging for deployment."
+	else
+		if [[ "$*" == *"--test"* ]]; then
+			noti "'--test' detected. Running backend tests."
+			cd "$LOCA/backend/" || exit 1
+			gleam test --target erlang || {
+				errnoti "\t--> Backend tests ran into an error."
+				exit 1
+			}
+			noti "'--test' detected. Running library tests."
+			cd "$LOCA/shared/" || exit 1
+			gleam test --target erlang || {
+				errnoti "\t--> Library tests ran into an error."
+				exit 1
+			}
+			noti "'--test' detected. Running frontend tests."
+			if [[ "$*" == *"--frontend-ts"* ]]; then
+				cd "$LOCA/frontend-ts/" || exit 1
+				bun test || {
+					errnoti "\t--> Frontend tests ran into an error."
+					exit 1
+				}
+			else
+				if [[ "$*" == *"--frontend-gleam"* ]]; then
+					cd "$LOCA/frontend/" || exit 1
+					gleam test --target javascript || {
+						errnoti "\t--> Frontend tests ran into an error."
+						exit 1
+					}
+				fi
 			fi
+			success "All tests completed."
+		fi
 	fi
-fi
-fi
 fi
