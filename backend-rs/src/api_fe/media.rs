5d57159c3c12a7c21da5bd050c01593128c7d74e
MLC Bloeiman <mar@strawmelonjuice.com>
2025-02-22 12:28:14 +0100

    Clean the slate.

--- backend-rs/src/api_fe/media.rs
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright (c) 2024, MLC 'Strawmelonjuice' Bloeiman
- *
- * Licensed under the BSD 3-Clause License. See the LICENSE file for more info.
- */
-
-//! This module handles media-related API endpoints, such as uploading media.
