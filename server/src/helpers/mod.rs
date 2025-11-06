ca7a298bb2158a3abd796bbc95a4714faa15dc36
MLC Bloeiman <mar@strawmelonjuice.com>
2025-11-06 23:29:37 +0100

    fix: correct typo in EventType::Success enum label

--- server/src/helpers/mod.rs
+++ server/src/helpers/mod.rs
@@ -17,7 +17,7 @@ pub fn message_prefixes() -> (
     let info = "[INFO]".color_green().style_bold();
     let warn = "[WARN]".color_yellow().style_bold();
     let error = "[ERROR]".color_error_red().style_bold();
-    let succes = "[✅ SUCCES]".color_ok_green().style_bold();
+    let succes = "[✅ SUCCESS]".color_ok_green().style_bold();
     let failure = "[✖️ FAILURE]".color_error_red().style_bold();
     let log = "[LOG]".color_blue().style_bold();
     let incoming = "[INCOMING]".color_lilac().style_bold();
