c69da1f6ec6d83b62f77511e98bfd7ea31afe2d2
MLC Bloeiman <mar@strawmelonjuice.com>
2026-02-21 17:57:07 +0100

    Remove /assets route mount

--- server/src/main.rs
+++ server/src/main.rs
@@ -335,7 +335,6 @@ async fn main() {
                                 staticroutes::favicon,
                             ],
                         )
-                        .mount("/assets", rocket::fs::FileServer::from("./assets"))
                         .manage(appstate)
                         .manage(rate_limiter)
                         .manage(auth_rate_limiter)
