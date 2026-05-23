b44b11ebe89fd4d20f3f1db98994aa08e1c506d9
MLC Bloeiman <mar@strawmelonjuice.com>
2026-05-23 09:49:51 +0200

    -- Iteration 3 --
    Signed-off-by: MLC Bloeiman <mar@strawmelonjuice.com>

--- README.md
+++ README.md
@@ -9,3 +9,21 @@ Lumina is a project in development, as the short description says "Just trying o
 way ready for you to try. However, you are encouraged to contribute in any way!
 
 Currently, as no stable is produced yet, code lives mainly in the `development` branch.
+
+
+## Development
+
+Use `flake.nix`, either using `direnv` (there's an `.envrc` file to do this!) or with `nix develop`.
+This gets you all the dependencies, including Just.
+
+```sh
+just dev # Prepares your enviroment and builds/runs the server with file watching.
+```
+
+When running Lumina server in development mode, it automatically creates two accounts for you and one of those has an attached
+post on the global timeline.
+
+| Username  | Email             | Password         |
+| --------- | ----------------- | ---------------- |
+| testuser1 | test@lumina123.co | MyTestPassw9292! |
+| testuser2 | test@lumina234.co | MyTestPassw9292! |
--- flake.nix
+++ flake.nix
@@ -1,33 +1,97 @@
 {
+  description = "Lumina Development Environment";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils }:
-    utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
+
+        myImage = pkgs.dockerTools.buildLayeredImage {
+          name = "luminapeonies";
+          tag = "latest";
+          contents = [
+            pkgs.bash
+            pkgs.coreutils
+            pkgs.erlang_28
+          ];
+          config = {
+            Cmd = [
+              "/app/entrypoint.sh"
+              "run"
+            ];
+            WorkingDir = "/data";
+            Env = [
+              "PATH=/usr/bin:/bin"
+              "PORT=3000"
+            ];
+          };
+          # Volumes = {
+          #   "/data" = { };
+          # };
+          extraCommands = ''
+            mkdir -p app
+            cp -r ${./server/build/erlang-shipment}/* app/
+            chmod +x app/entrypoint.sh
+          '';
+        };
       in
       {
+        packages.container = myImage;
+
         devShells.default = pkgs.mkShell {
+          shellHook = ''
+            echo "❄️ dev environment loaded, use 'just dev' next, or use 'just --list' for recipies."
+          '';
           buildInputs = with pkgs; [
+            # Gleam application
             gleam
             erlang_28
             rebar3
-            bun
+            deno
             tailwindcss_4
-            just
+            # Task runner
             watchexec
+            just
+            # Migrations
+            dbmate
+            sqlite
+            # Containerisation
             podman
           ];
-
-          shellHook = ''
-            echo "❄️ Welcome!"
-            # just --list # No just recipes yet.
-            # echo "Use just to run them."
-            gleam export javascript-prelude > web/prelude.mjs
-          '';
         };
-      });
+      }
+    )
+    // {
+      nixosModules.default =
+        {
+          config,
+          lib,
+          pkgs,
+          ...
+        }:
+        {
+          virtualisation.oci-containers.containers."strawmelonjuice-lumina" = {
+            # image = "strawmelonjuice/luminapeonies:latest";
+            image = "luminapeonies:latest";
+            ports = [
+              "3000:3000"
+            ];
+            volumes = [
+              "/var/lib/lumina-peonies:/data"
+            ];
+            extraOptions = [ "--network=slirp4netns" ];
+          };
+        };
+    };
 }
