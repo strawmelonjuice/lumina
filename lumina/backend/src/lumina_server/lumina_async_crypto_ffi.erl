72c011692de9574484ece5028b951f30f2a498da
MLC Bloeiman <mar@strawmelonjuice.com>
2026-07-14 10:46:41 +0200

    docs: Add commentary to async crypto ffi module
    This is actually part of a bigger change I'll introduce later, adding
    functions to convert an ed25519 to a x25519, allowing for private data
    transfer between individual users, hopefully.
    
    
    Signed-off-by: MLC Bloeiman <mar@strawmelonjuice.com>

--- lumina/backend/src/lumina_server/lumina_async_crypto_ffi.erl
+++ lumina/backend/src/lumina_server/lumina_async_crypto_ffi.erl
@@ -1,11 +1,18 @@
--module(lumina_async_crypto_ffi).
--export([generate_keypair/0, sign/2, verify/3]).
+%% **Lumina > Backend > Erlang Native Implementations >**
+%% # Asynchronous cryptography
+%%
 
+-module(lumina_async_crypto_ffi).
+-export([generate_keypair/0, sign/2, verify/3, ed25519_to_x25519_public/1, compute_x25519_shared_secret/2]).
+
+%% Generates an ed25519 keypair, used for the identification and signing of a Lumina user!
 generate_keypair() ->
     crypto:generate_key(eddsa, ed25519).
 
+%% Sign a message using ed25519.
 sign(Message, PrivateKey) ->
     crypto:sign(eddsa, none, Message, [PrivateKey, ed25519]).
 
+%% Verify a signature on a ed25519 signed message.
 verify(Message, Signature, PublicKey) ->
     crypto:verify(eddsa, none, Message, Signature, [PublicKey, ed25519]).
